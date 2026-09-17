cfa9a966d7c575dbe43f2c548b005d48be51f7fd
Aleksander Polek <twoj-email@example.com>
2026-09-17 19:59:33 +0200

    add SecurityConfig with method Password Encoder and SecurityFilterChain

--- vetcare-backend/src/main/java/com/vetcare/backend/config/SecurityConfig.java
+++ vetcare-backend/src/main/java/com/vetcare/backend/config/SecurityConfig.java
@@ -1,4 +1,21 @@
 package com.vetcare.backend.config;
 
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/users/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
+
